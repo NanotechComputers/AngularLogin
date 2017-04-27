326d04ab78223cadb5bc1faccb11f774b50a4c79
Kevin Deason <kevin@deason.co.za>
2017-04-27 16:36:43 +0200

    feat: Add login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {environment} from "../environments/environment";
+import {OAuthService} from "./services/oauthservice/auth-service.service";
+
+const TOKEN_BASE_URL = environment.tokenBaseUrl;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private oauthService: OAuthService) {
+    let locationOrigin = window.location.origin;
+    this.oauthService.loginUrl = `${locationOrigin}/login`;
+    this.oauthService.logoutUrl = `${locationOrigin}/login`;
+    this.oauthService.tokenEndpoint = `${TOKEN_BASE_URL}/api-token-auth/`; //Why does it require a / at the end? this means it's a directory not a method.
+    this.oauthService.tryLogin({});
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {OAuthService} from "../../services/oauthservice/auth-service.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: []
+  styles: [`
+    .btn-login, input {
+      height: 40px;
+    }
+  `]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  userName: string = '';
+  password: string = '';
+  loginFailed: boolean = false;
 
-  ngOnInit() {
+  constructor(protected router: Router, private oAuthService: OAuthService) {
   }
 
+  public login() {
+    this.oAuthService.redirectUri = window.location.origin + "/app";
+    this
+      .oAuthService
+      .fetchTokenUsingPasswordFlow(this.userName, this.password)
+      .then(() => {
+        var hasAccessToken = this.oAuthService.hasValidAccessToken();
+
+        if (hasAccessToken) {
+          //noinspection JSIgnoredPromiseFromCall
+          this.router.navigate(['/app']);
+        }
+        this.loginFailed = false;
+      })
+      .catch((err) => {
+        console.error('error logging in', err);
+        this.loginFailed = true;
+      })
+  }
 }
