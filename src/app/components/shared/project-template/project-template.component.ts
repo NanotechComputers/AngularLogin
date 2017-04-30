5221fddb0bcaa4908acdda280f4b70c3549b99d8
Kevin Deason <kevin@deason.co.za>
2017-04-30 20:10:13 +0200

    feat: Add component for project listing template

---
+++ src/app/components/shared/project-template/project-template.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {TaskModel} from "../../../models/taskset.model";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'project',
+  templateUrl: './project-template.component.html',
+  styleUrls: ['./project-template.component.css']
+})
+export class ProjectTemplateComponent {
+  constructor(private router:Router, private route: ActivatedRoute){}
+
+  @Input('projectItem') data: number;
+
+  public editProject(id:number){
+    //noinspection JSIgnoredPromiseFromCall
+    console.log(id);
+    this.router.navigate(['/app/project', id]);
+  }
+
+  public deleteProject(id:number){
+    alert(`Not implemented. deleteProject(${id})`)
+  }
+
+  public setActive(id:number){
+    alert(`Not implemented. setActive (${id})`)
+  }
+
+  public setBillable(id:number){
+    alert(`Not implemented. setBillable(${id})`)
+  }
+}
