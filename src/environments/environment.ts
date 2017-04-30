72feda01d262252ad5500ebc7e4814a09f2cbe81
Kevin Deason <kevin@deason.co.za>
2017-04-30 20:11:44 +0200

    feat: Add environment variable for projects url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  tokenBaseUrl: 'http://userservice.staging.tangentmicroservices.com'
+  tokenBaseUrl: 'http://userservice.staging.tangentmicroservices.com',
+  apiBaseUrl: 'http://projectservice.staging.tangentmicroservices.com/api/v1/'
 };
