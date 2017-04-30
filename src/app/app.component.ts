8238ca7b79f7cc6f70461357af5c0140cce52dab
Kevin Deason <kevin@deason.co.za>
2017-04-30 20:07:32 +0200

    feat: Add preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {environment} from "../environments/environment";
 import {OAuthService} from "./services/oauthservice/auth-service.service";
 
@@ -7,14 +7,29 @@ const TOKEN_BASE_URL = environment.tokenBaseUrl;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '[class.loaded]': 'isLoaded',
+    '[class.faded]': 'faded',
+  }
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   constructor(private oauthService: OAuthService) {
     let locationOrigin = window.location.origin;
     this.oauthService.loginUrl = `${locationOrigin}/login`;
     this.oauthService.logoutUrl = `${locationOrigin}/login`;
     this.oauthService.tokenEndpoint = `${TOKEN_BASE_URL}/api-token-auth/`; //Why does it require a / at the end? this means it's a directory not a method.
     this.oauthService.tryLogin({});
+  }
+
+  public isLoaded:boolean = false;
+  public faded:boolean = false;
+  ngAfterViewInit() {
+    setTimeout(() => this.hidePreloader(), 600);
+  }
+
+  hidePreloader(){
+    this.isLoaded = true;
+    setTimeout(() => this.faded = true, 1300);
   }
 }
